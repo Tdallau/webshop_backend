75e481d88b6caa14697e187fbaed38e5bfb52277
Tim Dallau <timdallau@MacBookProTim.private.dallau.eu>
2018-09-20 22:20:01 +0200

    fixed cors

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,13 @@ namespace webshop_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -40,7 +47,9 @@ namespace webshop_backend
                 app.UseHsts();
             }
 
+
             app.UseHttpsRedirection();
+            app.UseCors("MyPolicy");
             app.UseMvc();
         }
     }
