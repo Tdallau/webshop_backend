848f20556e9771bc8d1d242495f257254c4caf54
Tim Dallau <timdallau@MacBookProTim.private.dallau.eu>
2018-09-24 17:45:30 +0200

    added the Admin and User Role

--- Models/User.cs
+++ Models/User.cs
@@ -8,6 +8,7 @@ namespace Models
         public string Email {get; set;}
         public string Name {get; set;}
         public string Gender {get; set;}
+        public string Role {get; set;}
         public string Password {get; set;}
         public string Salt {get; set;}
         public List<Addresses> Addresses {get; set;}
