03c3ad91a6439ff023576ae34dec8f8e16e24a1e
lenscas <lenscas@lenscas-laptop-2>
2018-11-12 11:52:40 +0100

    Fix wrong link for pictures

--- Migrations/20181112105121_Updated ImageUrlLink.Designer.cs
+++ Migrations/20181112105121_Updated ImageUrlLink.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace webshop_backend.Migrations
 {
     [DbContext(typeof(MainContext))]
-    partial class MainContextModelSnapshot : ModelSnapshot
+    [Migration("20181112105121_Updated ImageUrlLink")]
+    partial class UpdatedImageUrlLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20181112105121_Updated ImageUrlLink.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webshop_backend.Migrations
+{
+    public partial class UpdatedImageUrlLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImagesUrl_Print_printId",
+                table: "ImagesUrl");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImagesUrl_printId",
+                table: "ImagesUrl");
+
+            migrationBuilder.DropColumn(
+                name: "printId",
+                table: "ImagesUrl");
+
+            migrationBuilder.AddColumn<int>(
+                name: "printFaceid",
+                table: "ImagesUrl",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImagesUrl_printFaceid",
+                table: "ImagesUrl",
+                column: "printFaceid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImagesUrl_PrintFace_printFaceid",
+                table: "ImagesUrl",
+                column: "printFaceid",
+                principalTable: "PrintFace",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImagesUrl_PrintFace_printFaceid",
+                table: "ImagesUrl");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImagesUrl_printFaceid",
+                table: "ImagesUrl");
+
+            migrationBuilder.DropColumn(
+                name: "printFaceid",
+                table: "ImagesUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "printId",
+                table: "ImagesUrl",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImagesUrl_printId",
+                table: "ImagesUrl",
+                column: "printId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImagesUrl_Print_printId",
+                table: "ImagesUrl",
+                column: "printId",
+                principalTable: "Print",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
